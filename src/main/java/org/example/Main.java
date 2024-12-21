d46806c9857bda79a59036e7a57932eed96b431b
ivan <anime210404@gmail.com>
2024-12-21 16:31:12 +0200

    add converter logic

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
 public class Main {
+    private static final double CONV_K = 2.20462;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Convert App.");
+        double kgs = 5;
+        double pounds = convertKgsToPounds(kgs);
+        System.out.println("Result: " + pounds + " pounds.");
+    }
+    private static double convertKgsToPounds(double kgs) {
+        return kgs * CONV_K;
     }
 }
