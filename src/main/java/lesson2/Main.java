4d0b4ee6e1ba9a36ab93a8c659653ef0f3268a94
ivan <anime210404@gmail.com>
2025-01-07 18:21:14 +0200

    create convert km to mile logic

--- src/main/java/lesson2/Main.java
+++ src/main/java/lesson2/Main.java
@@ -4,8 +4,12 @@ public class Main {
     private static final double coefficient = 1.609;
     public static void main(String[] args) {
         System.out.println(convertMileToKm(5));
+        System.out.println(convertKmToMile(8));
     }
     private static double convertMileToKm(double mile) {
         return mile * coefficient;
+    }
+    private static double convertKmToMile(double km) {
+        return km / coefficient;
     }
 }
