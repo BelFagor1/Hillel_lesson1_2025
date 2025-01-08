f860099e6453e1f05f5f54b7942636fc8ce4803e
ivan <anime210404@gmail.com>
2025-01-08 17:01:19 +0200

    Add converter celsius to far

--- src/main/java/lesson3/Main.java
+++ src/main/java/lesson3/Main.java
@@ -2,6 +2,9 @@ package lesson3;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(convertCelsiusToFahrenheit(4));
+    }
+    public static double convertCelsiusToFahrenheit(double celsius) {
+        return  celsius * 1.8 + 32;
     }
 }
