86cc0878721280b29a25aceddb6e9e829716a531
ivan <anime210404@gmail.com>
2025-02-06 21:06:56 +0200

    homework lesson 10 is done

---
+++ src/main/java/lesson10/FileHandler.java
@@ -0,0 +1,32 @@
+package lesson10;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileHandler {
+
+    private final static String BASE_PATH = "files/";
+
+    public String writeFile(String fileName, String fileContent) {
+        try(FileWriter fw = new FileWriter(BASE_PATH + fileName + ".txt")) {
+            fw.write(fileContent);
+            return "Success.";
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    public String readFile(String path) {
+        try (FileReader reader = new FileReader(path)) {
+            int sym;
+            StringBuilder ff = new StringBuilder();
+            while ((sym = reader.read()) != -1) {
+                ff.append((char) sym);
+            }
+            return ff.toString();
+        } catch (IOException ex) {
+            return ex.getMessage();
+        }
+    }
+}
